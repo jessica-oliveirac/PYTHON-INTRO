6ea6b3cb3aa5c36f0eb3ecbf9d09473f8ad5e7ca
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2022-11-22 16:14:34 -0300

    Create ex08.py

---
+++ ex08.py
@@ -0,0 +1,13 @@
+#8)Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado. Para ser aprovado, todas as médias do aluno devem ser maiores que 7. 
+#Considere que o aluno cursa apenas três matérias, e que a nota de cada uma está armazenada nas seguintees variáveis: matéria1, matéria2 e matéria3
+
+materia1=8
+materia2=5
+materia3=7
+
+media=(8+5+7)/3
+
+if media>7:
+    print("Aprovado")
+else:
+    print("Reprovado")
