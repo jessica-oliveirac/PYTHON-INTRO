079bc4d5be26ac1498fd604eb5a2d7ecf3a3dcda
Jéssica <90432259+jessica-oliveirac@users.noreply.github.com>
2022-11-28 01:28:06 -0300

    Delete ex03.py

--- ex03.py
+++
@@ -1,8 +0,0 @@
-#3)Escreva um programa que leia três números e que imprima o maior e o menor.
-
-N1=int(input("Digite um nº: "))
-N2=int(input("Digite outro nº: "))
-N3=int(input("Digite mais um nº: "))
-
-print('Maior Nº: ',max(N1,N2,N3))
-print('Menor Nº: ', min(N1, N2, N3))
